fc3fa8f84975d964cd4e8cf54ac797175e9c3505
Shivam Yadav <shivam.coder01@gmail.com>
2025-06-07 11:55:47 +0530

    changed border color

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ export default function About() {
     <section id="about" className="py-16 px-2 md:px-6 lg:mx-12">
       <h2 className="text-2xl mb-8 text-center px-6 py-2 w-fit rounded-full mx-auto bg-yellow-400 text-gray-800">About Me</h2>
 
-      <div className="rounded-[2rem] sm:rounded-[6rem] border-2 border-gray-800 bg-white max-w-[1000px] mx-auto ">
+      <div className="rounded-[2rem] sm:rounded-[6rem] border-2 border-gray-400 bg-white max-w-[1000px] mx-auto ">
 
         {/* Header */}
         <Header />
